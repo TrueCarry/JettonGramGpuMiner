9aa8958b1982ac4546a1f9efea8b1c7b4809ea31
Tim <truecarry@gmail.com>
2024-02-02 17:17:41 +0300

    Check if miner working before starting to mine

--- send_universal.ts
+++ send_universal.ts
@@ -136,6 +136,14 @@ let go = true
 let i = 0
 let lastMinedSeed: bigint = BigInt(0)
 async function main() {
+    const minerOk = await testMiner()
+    if (!minerOk) {
+        console.log('Your miner is not working')
+        console.log('Check if you use correct bin (cuda, amd).')
+        console.log('If it doesn\'t help, try to run test_cuda or test_opencl script, to find out issue')
+        process.exit(1)
+    }
+
     let liteClient: ApiObj
     if (!args['--api']) {
         console.log('Using TonHub API')
@@ -164,6 +172,8 @@ async function main() {
     } else {
         console.log('Using v4r2 wallet', wallet.address.toString({ bounceable: false, urlSafe: true }))
     }
+
+
 
     try {
         await updateBestGivers(liteClient, wallet.address)
@@ -321,6 +331,28 @@ async function sendMinedBoc(
             }
         }
     }
+}
+
+async function testMiner(): Promise<boolean> {
+    const randomName = (await getSecureRandomBytes(8)).toString('hex') + '.boc'
+    const path = `bocs/${randomName}`
+    const command = `${bin} -g ${gpu} -F 128 -t ${timeout} kQBWkNKqzCAwA9vjMwRmg7aY75Rf8lByPA9zKXoqGkHi8SM7 229760179690128740373110445116482216837 53919893334301279589334030174039261347274288845081144962207220498400000000000 10000000000 kQBWkNKqzCAwA9vjMwRmg7aY75Rf8lByPA9zKXoqGkHi8SM7 ${path}`
+    try {
+        const output = execSync(command, { encoding: 'utf-8', stdio: "pipe" });  // the default is 'buffer'
+    } catch (e) {
+    }
+    let mined: Buffer | undefined = undefined
+    try {
+        mined = fs.readFileSync(path)
+        fs.rmSync(path)
+    } catch (e) {
+        //
+    }
+    if (!mined) {
+        return false
+    }
+    
+    return true
 }
 
 
