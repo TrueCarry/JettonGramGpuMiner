dd2e9abc16598a9bc573dbaeeef4bfeaf59ba43f
Tim <truecarry@gmail.com>
2024-02-03 01:36:12 +0300

    Fix multi typo

--- start_multi_8.sh
+++ start_multi_8.sh
@@ -3,6 +3,6 @@ npm install
 
 
 while true; do
-  node send_multiple.js --api tonapi --bin ./pow-miner-cuda --givers 1000 --gpu-count 8
+  node send_multigpu.js --api tonapi --bin ./pow-miner-cuda --givers 1000 --gpu-count 8
   sleep 1;
 done;
