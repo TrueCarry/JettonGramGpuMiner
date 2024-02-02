24f51a342e24d530427d4fc1da17f19ee4f4b05d
Tim <truecarry@gmail.com>
2024-02-02 22:28:57 +0300

    Replace else with return

--- send_universal.ts
+++ send_universal.ts
@@ -271,7 +271,6 @@ async function sendMinedBoc(
     giverAddress: string,
     boc: Cell
 ) {
-
     if (args['--api'] === 'tonapi') {
         const tonapiClient = await getTonapiClient()
 
@@ -315,37 +314,38 @@ async function sendMinedBoc(
 
             }
         }
-    } else {
-        const wallets: OpenedContract<WalletContractV4>[] = []
-        const ton4Client = await getTon4Client()
-        const tonOrbsClient = await getTon4ClientOrbs()
-        const w2 = ton4Client.open(wallet)
-        const w3 = tonOrbsClient.open(wallet)
-        wallets.push(w2)
-        wallets.push(w3)
+        return
+    }
 
-        if (args['--api'] === 'lite') {
-            const liteServerClient = await getLiteClient(args['-c'] ?? 'https://ton-blockchain.github.io/global.config.json')
-            const w1 = liteServerClient.open(wallet)
-            wallets.push(w1)
-        }
+    const wallets: OpenedContract<WalletContractV4>[] = []
+    const ton4Client = await getTon4Client()
+    const tonOrbsClient = await getTon4ClientOrbs()
+    const w2 = ton4Client.open(wallet)
+    const w3 = tonOrbsClient.open(wallet)
+    wallets.push(w2)
+    wallets.push(w3)
 
-        for (let i = 0; i < 3; i++) {
-            for (const w of wallets) {
-                w.sendTransfer({
-                    seqno,
-                    secretKey: keyPair.secretKey,
-                    messages: [internal({
-                        to: giverAddress,
-                        value: toNano('0.05'),
-                        bounce: true,
-                        body: boc,
-                    })],
-                    sendMode: 3 as any,
-                }).catch(e => {
-                    //
-                })
-            }
+    if (args['--api'] === 'lite') {
+        const liteServerClient = await getLiteClient(args['-c'] ?? 'https://ton-blockchain.github.io/global.config.json')
+        const w1 = liteServerClient.open(wallet)
+        wallets.push(w1)
+    }
+
+    for (let i = 0; i < 3; i++) {
+        for (const w of wallets) {
+            w.sendTransfer({
+                seqno,
+                secretKey: keyPair.secretKey,
+                messages: [internal({
+                    to: giverAddress,
+                    value: toNano('0.05'),
+                    bounce: true,
+                    body: boc,
+                })],
+                sendMode: 3 as any,
+            }).catch(e => {
+                //
+            })
         }
     }
 }
