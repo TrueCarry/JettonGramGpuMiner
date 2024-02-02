134f1084eb24b9d753e398b55f21fa2242bd3421
Kesha Antonov <innokenty.longway@gmail.com>
2024-02-02 21:10:47 +0300

    removed extra calls to wallets

--- send_universal.ts
+++ send_universal.ts
@@ -272,20 +272,6 @@ async function sendMinedBoc(
     boc: Cell
 ) {
 
-    const wallets: OpenedContract<WalletContractV4>[] = []
-    const ton4Client = await getTon4Client()
-    const tonOrbsClient = await getTon4ClientOrbs()
-    const w2 = ton4Client.open(wallet)
-    const w3 = tonOrbsClient.open(wallet)
-    wallets.push(w2)
-    wallets.push(w3)
-
-    if (args['--api'] === 'lite') {
-        const liteServerClient = await getLiteClient(args['-c'] ?? 'https://ton-blockchain.github.io/global.config.json')
-        const w1 = liteServerClient.open(wallet)
-        wallets.push(w1)
-    }
-
     if (args['--api'] === 'tonapi') {
         const tonapiClient = await getTonapiClient()
 
@@ -329,23 +315,39 @@ async function sendMinedBoc(
 
             }
         }
-    } else {
-        for (let i = 0; i < 3; i++) {
-            for (const w of wallets) {
-                w.sendTransfer({
-                    seqno,
-                    secretKey: keyPair.secretKey,
-                    messages: [internal({
-                        to: giverAddress,
-                        value: toNano('0.05'),
-                        bounce: true,
-                        body: boc,
-                    })],
-                    sendMode: 3 as any,
-                }).catch(e => {
-                    //
-                })
-            }
+
+        return
+    }
+
+    const wallets: OpenedContract<WalletContractV4>[] = []
+    const ton4Client = await getTon4Client()
+    const tonOrbsClient = await getTon4ClientOrbs()
+    const w2 = ton4Client.open(wallet)
+    const w3 = tonOrbsClient.open(wallet)
+    wallets.push(w2)
+    wallets.push(w3)
+
+    if (args['--api'] === 'lite') {
+        const liteServerClient = await getLiteClient(args['-c'] ?? 'https://ton-blockchain.github.io/global.config.json')
+        const w1 = liteServerClient.open(wallet)
+        wallets.push(w1)
+    }
+
+    for (let i = 0; i < 3; i++) {
+        for (const w of wallets) {
+            w.sendTransfer({
+                seqno,
+                secretKey: keyPair.secretKey,
+                messages: [internal({
+                    to: giverAddress,
+                    value: toNano('0.05'),
+                    bounce: true,
+                    body: boc,
+                })],
+                sendMode: 3 as any,
+            }).catch(e => {
+                //
+            })
         }
     }
 }
