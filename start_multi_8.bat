804461c1b9bb44c4c8dcbea430e823a8bc007076
Tim <truecarry@gmail.com>
2024-02-03 01:36:12 +0300

    Fix multi typo

--- start_multi_8.bat
+++ start_multi_8.bat
@@ -2,7 +2,7 @@
 call ".\npm_install.bat"
 
 :_minerstart
-node send_multiple.js --api tonapi --givers 1000 --gpu-count 8
+node send_multigpu.js --api tonapi --givers 1000 --gpu-count 8
 goto _minerstart
 
 pause
